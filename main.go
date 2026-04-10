41fd6946eb8a883fc9b5a6febf87617b0354da9e
deannos <amjha21122002@gmail.com>
2026-04-11 00:09:40 +0530

    feat: graceful shutdown on SIGTERM/SIGINT with 10s drain and db close
    Closes #8

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/deannos/notification-queue/config"
 	"github.com/deannos/notification-queue/db"
@@ -29,10 +35,33 @@ func main() {
 	h := hub.New()
 	go h.Run()
 
-	r := router.Setup(database, h, cfg)
-
-	log.Printf("NotifyQ listening on %s", cfg.ListenAddr)
-	if err := r.Run(cfg.ListenAddr); err != nil {
-		log.Fatalf("server error: %v", err)
+	srv := &http.Server{
+		Addr:    cfg.ListenAddr,
+		Handler: router.Setup(database, h, cfg),
 	}
+
+	go func() {
+		log.Printf("NotifyQ listening on %s", cfg.ListenAddr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("server error: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("forced shutdown: %v", err)
+	}
+
+	if sqlDB, err := database.DB(); err == nil {
+		sqlDB.Close()
+	}
+
+	log.Println("shutdown complete")
 }
